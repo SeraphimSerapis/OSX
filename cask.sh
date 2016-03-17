3365879b14afd93b415e08e4670659ca2e5831c8
Tim Messerschmidt <timmesserschmidt@gmail.com>
2016-03-17 19:49:49 +0100

    fixed a missing new line

--- cask.sh
+++ cask.sh
@@ -5,17 +5,18 @@ brew cask install cheatsheet
 brew cask install imageoptim
 brew cask install istat-menus
 brew cask install google-chrome
-brew cask install spotify atom
+brew cask install google-photos-backup
+brew cask install google-drive
+brew cask install google-hangouts
+brew cask install spotify
+brew cask install atom
 brew cask install slack
 brew cask install alfred
 brew cask install bartender
 brew cask install spectacle
-brew cask install google-photos-backup
-brew cask install google-drive
 brew cask install android-studio
+brew cask install androidtool
 brew cask install iterm2
 brew cask install caffeine
-brew cask install google-hangouts
 brew cask install 1password
-brew cask install androidtool
 brew cask install mou
