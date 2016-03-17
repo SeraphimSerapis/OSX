934ca1302c6c4634081e2672d694a28ea2dc2de0
Tim Messerschmidt <timmesserschmidt@gmail.com>
2016-03-17 19:33:38 +0100

    Better cask.sh

--- cask.sh
+++ cask.sh
@@ -1,3 +1,20 @@
 # #!/bin/bash
 
-brew cask install java istat-menus google-chrome spotify atom slack alfred bartender spectacle google-photos-backup google-drive android-studio iterm2 caffeine google-hangouts 1password androidtool mou
+brew cask install java
+brew cask install imageoptim
+brew cask install istat-menus
+brew cask install google-chrome
+brew cask install spotify atom
+brew cask install slack
+brew cask install alfred
+brew cask install bartender
+brew cask install spectacle
+brew cask install google-photos-backup
+brew cask install google-drive
+brew cask install android-studio
+brew cask install iterm2
+brew cask install caffeine
+brew cask install google-hangouts
+brew cask install 1password
+brew cask install androidtool
+brew cask install mou
