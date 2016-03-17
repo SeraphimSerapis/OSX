996353f50bc286180ef541e287fe02720098c32b
Tim Messerschmidt <timmesserschmidt@gmail.com>
2016-03-17 19:22:00 +0100

    Adds cask install file

---
+++ cask.sh
@@ -0,0 +1,3 @@
+# #!/bin/bash
+
+brew cask install java istat-menus google-chrome spotify atom slack alfred bartender spectacle google-photos-backup google-drive android-studio iterm2 caffeine google-hangouts 1password androidtool mou
